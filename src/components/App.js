6ba5a67b69d556001ef95e073e1be60839949bff
David Baden <davebaden@gmail.com>
2023-11-27 17:14:14 -0800

    pass tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App () { 
+
+    const [dogPic, setDogPic] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then((data) => {
+            setDogPic(data.message);
+            setIsLoaded(true);
+        })
+    }, []);
+
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+
+  return (
+    <div>
+        <p><img src={dogPic} alt="A Random Dog" /></p>
+    </div>
+  )
+}
+
+export default App
+
+
